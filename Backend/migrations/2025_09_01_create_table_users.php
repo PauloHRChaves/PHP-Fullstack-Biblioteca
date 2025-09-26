547139be255ffd98a322fd6bdf23d4033eca67ed
PauloHRChaves <hpaulochaves@gmail.com>
2025-09-26 15:51:16 -0300

    fix: concertada conexão com banco de dados em create_table_users

--- Backend/migrations/2025_09_01_create_table_users.php
+++ Backend/migrations/2025_09_01_create_table_users.php
@@ -1,5 +1,14 @@
 <?php
-require __DIR__ . '/../src/database.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+require_once __DIR__ . '/../src/config/database.php'; 
+
+$pdo = connectToDatabase();
 
 date_default_timezone_set('America/Sao_Paulo');
 
